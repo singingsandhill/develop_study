2589788a662910bf94bcf317bdee2971dd428cb3
jisu <jisu0259@gmail.com>
2025-01-10 17:23:59 +0900

    Test: order service test

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -23,4 +25,32 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    /**
+     * new orderItem 을 막는 생성자
+     * 쓰는 곳에서 생성자를 만들고 주입하면 유지보수가 어려움
+     * @NoArgsConstructor(access = AccessLevel.PROTECTED)로 대체 가능
+     */
+    protected OrderItem() {}
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStockQuantity(count);
+
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        getItem().addStockQuantity(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
