842aa2dbd92f9acb36224c7592ff3f5597b9e93a
jisu <jisu0259@gmail.com>
2025-01-12 18:18:44 +0900

    CS: MVC 패턴

---
+++ spring-prepare/src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepare;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class Memo {
+    private String username;
+    private String contents;
+
+}
