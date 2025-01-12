3a42c1d78c6f6f22566c76aed8eb3957e358c459
jisu <jisu0259@gmail.com>
2025-01-13 00:20:00 +0900

    Feat: rest controller

---
+++ spring-prepare/src/main/java/com/sparta/springprepare/controller/ResponseRestController.java
@@ -0,0 +1,30 @@
+package com.sparta.springprepare.controller;
+
+import com.sparta.springprepare.Star;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/response/rest")
+public class ResponseRestController {
+
+        // [Response header]
+        //   Content-Type: text/html
+        // [Response body]
+        //   {"name":"Robbie","age":95}
+        @GetMapping("/json/string")
+        public String helloStringJson() {
+            return "{\"name\":\"Robbie\",\"age\":95}";
+        }
+
+        // [Response header]
+        //   Content-Type: application/json
+        // [Response body]
+        //   {"name":"Robbie","age":95}
+        @GetMapping("/json/class")
+        public Star helloClassJson() {
+            return new Star("Robbie", 95);
+        }
+
+}
