5d4360163e1c681702d62c49992f4700273b644e
jisu <jisu0259@gmail.com>
2025-01-13 22:27:43 +0900

    create read 구현

---
+++ memo/src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,23 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Memo {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+}
