61b32a3b92fd8ada728e15a3b5ecaf8c0020d36f
jisu <jisu0259@gmail.com>
2025-01-14 23:23:36 +0900

    Refactor: 3 layer

--- memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -1,4 +1,58 @@
 package com.sparta.memo.service;
 
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.repsitory.MemoRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MemoService {
+
+    private final JdbcTemplate jdbcTemplate;
+    public final MemoRepository memoRepository;
+
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // DB 저장
+        Memo saveMemo = memoRepository.save(memo);
+
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+
+        return memoRepository.findAll();
+
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+
+        return memoRepository.update(id, requestDto);
+    }
+
+    public Long deleteMemo(Long id) {
+
+        return memoRepository.delete(id);
+    }
+
 }
