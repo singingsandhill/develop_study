d542b5b4f5677f237a6bb62d2aa3c7415baba915
jisu <jisu0259@gmail.com>
2025-01-17 15:19:12 +0900

    JPA query method

--- memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -1,7 +1,6 @@
 package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +11,5 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
 
     List<Memo> findAllByOrderByModifiedAtDesc();
 
+    List<Memo> findAllByUsername(String username);
 }
