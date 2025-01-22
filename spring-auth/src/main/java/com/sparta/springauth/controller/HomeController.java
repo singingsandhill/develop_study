cfef51b8babb9c04a148b4a738a1714c41417382
jisu <jisu0259@gmail.com>
2025-01-22 18:35:15 +0900

    회원가입 구현

---
+++ spring-auth/src/main/java/com/sparta/springauth/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.sparta.springauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("username", "username");
+        return "index";
+    }
+}
