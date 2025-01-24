d59a37a3365f7616531ada6ec7dc856c99ad7f6e
jisu <jisu0259@gmail.com>
2025-01-24 18:37:13 +0900

    spring security 로그인

--- spring-auth/src/main/java/com/sparta/springauth/controller/HomeController.java
+++ spring-auth/src/main/java/com/sparta/springauth/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.sparta.springauth.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("username", "username");
+    public String home(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        if (userDetails != null) {
+            model.addAttribute("username", userDetails.getUsername());
+        }
+        log.info(String.valueOf(userDetails));
         return "index";
+    }
+
+    @GetMapping("/api/user/status")
+    @ResponseBody
+    public ResponseEntity<String> checkLoginStatus(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails != null) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(401).build();
     }
 }
--- spring-auth/src/main/resources/static/js/basic.js
+++ spring-auth/src/main/resources/static/js/basic.js
@@ -1,14 +1,16 @@
-let host = 'http://' + window.location.host;
-
 $(document).ready(function () {
     const auth = getToken();
     if(auth === '') {
-        window.location.href = host + "/api/user/login-page";
+        $('#login-true').show();
+        $('#login-false').hide();
+        // window.location.href = host + "/api/user/login-page";
     } else {
         $('#login-true').show();
         $('#login-false').hide();
     }
 })
+
+let host = 'http://' + window.location.host;
 
 function logout() {
     // 토큰 삭제
