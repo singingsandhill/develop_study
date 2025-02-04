dca0d6b93504ce9f74940882800c4cd62f4af4c0
jisu <jisu0259@gmail.com>
2025-02-04 15:24:05 +0900

    schehuler 기능 구현

--- myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -49,5 +50,12 @@ public class ProductService {
             productResponseDtoList.add(new ProductResponseDto(product));
         }
         return productResponseDtoList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(Math.toIntExact(id)).orElseThrow(()->
+                new NullPointerException("해당 상품은 존재하지 않습니다."));
+        product.updateByItemDto(itemDto);
     }
 }
