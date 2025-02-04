652a687f7c2615dbd0a6ad4d84c6fa42a4800197
jisu <jisu0259@gmail.com>
2025-02-04 11:55:47 +0900

    중간 테이블 order 주문일 추가

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -3,21 +3,31 @@ package com.sparta.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "food_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id", nullable = false)
     private Food food;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime orderDate;
+
 }
