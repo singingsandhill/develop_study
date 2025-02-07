223499934e129e3ee3d943bb962ab764be0c3067
singingsandhill <jisu0259@naver.com>
2025-02-07 14:25:36 +0900

    Top 5 회원 찾기 구현 (scratch)

--- myselectshop/src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/naver/controller/NaverApiController.java
@@ -19,7 +19,7 @@ public class NaverApiController {
     private final NaverApiService naverApiService;
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestParam String query)  {
+    public List<ItemDto> searchItems(@RequestParam("query") String query)  {
         return naverApiService.searchItems(query);
     }
 }
