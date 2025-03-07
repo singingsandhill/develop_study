81aaef36c235bf6499311da39c1d47fab82bdbf2
Kim jisu <jisu0259@naver.com>
2025-03-07 14:56:20 +0900

    custom redis template

--- com.example.redis/src/test/java/com/example/redis/RedisemplateTests.java
+++ com.example.redis/src/test/java/com/example/redis/RedisemplateTests.java
@@ -3,6 +3,7 @@ package com.example.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -16,21 +17,42 @@ public class RedisemplateTests {
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void stringOpsTest(){
-        ValueOperations<String,String> ops
+    public void stringOpsTest() {
+        ValueOperations<String, String> ops
                 // 지금 redisTemplate에 설정된 타입을 바탕으로 Redis에 문자열 조작
-                 = stringRedisTemplate.opsForValue();
-        ops.set("simpleKey","simpleValue");
+                = stringRedisTemplate.opsForValue();
+        ops.set("simpleKey", "simpleValue");
         System.out.println("simplekey");
 
 //        집합을 조작하기 위한 클래스
-        SetOperations<String,String> setOps
+        SetOperations<String, String> setOps
                 = stringRedisTemplate.opsForSet();
-        setOps.add("hobbies","games");
-        setOps.add("hobbies","coding","alcohol","games");
+        setOps.add("hobbies", "games");
+        setOps.add("hobbies", "coding", "alcohol", "games");
         System.out.println(setOps.size("hobbies"));
 
-        stringRedisTemplate.expire("hobbies",10, TimeUnit.SECONDS);
+        stringRedisTemplate.expire("hobbies", 10, TimeUnit.SECONDS);
         stringRedisTemplate.delete("simpleKey");
+    }
+
+    @Autowired
+    private RedisTemplate<String, ItemDto> itemRedisTemplate;
+
+    @Test
+    public void itemRedisTemplateTest() {
+        ValueOperations<String, ItemDto> ops = itemRedisTemplate.opsForValue();
+        ops.set("my:keyboard", ItemDto.builder()
+                .name("Mechanical keyboard")
+                .price(200000)
+                .description("good")
+                .build());
+
+        System.out.println(ops.get("my:keyboard"));
+
+        ops.set("my:mouse", ItemDto.builder()
+                .name("mouse")
+                .price(10000)
+                .description("OMG")
+                .build());
     }
 }
