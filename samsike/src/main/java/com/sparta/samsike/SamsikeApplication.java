225a75554859feeb3d01e811cb1131e0fb5778e2
Kim jisu <jisu0259@naver.com>
2025-03-07 11:08:03 +0900

    java 단기 심화 첫번째 프로젝트

---
+++ samsike/src/main/java/com/sparta/samsike/SamsikeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.samsike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsikeApplication.class, args);
+    }
+
+}
