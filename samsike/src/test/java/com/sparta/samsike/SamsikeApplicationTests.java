1144e5c8956a62f1c9ea8b65e8c0402ba2a469dd
Kim jisu <jisu0259@naver.com>
2025-03-07 11:08:03 +0900

    java 단기 심화 첫번째 프로젝트

---
+++ samsike/src/test/java/com/sparta/samsike/SamsikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.samsike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
